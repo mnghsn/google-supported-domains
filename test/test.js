606f56ecd4b5640516d580483750c4323da43e28
Jimmy Lin <jmlntw@gmail.com>
2022-07-13 09:55:27 +0800

    Avoid duplicatePrefix in test script

--- test/test.js
+++ test/test.js
@@ -1,36 +1,40 @@
 const googleSupportedDomains = require('../')
 
-test('validate all google domains', () => {
-  const domains = googleSupportedDomains.all()
-  const pattern = /^\.google\.[a-z.]+$/
+describe('validate domains', () => {
+  test('all google domains', () => {
+    const domains = googleSupportedDomains.all()
+    const pattern = /^\.google\.[a-z.]+$/
 
-  for (const domain of domains) {
-    expect(pattern.test(domain)).toBeTruthy()
-  }
+    for (const domain of domains) {
+      expect(pattern.test(domain)).toBeTruthy()
+    }
+  })
 })
 
-test('test main domain', () => {
-  const expected = {
-    'google.com': true,
-    'google.co.jp': true,
-    'example.com': false,
-    'example.net': false
-  }
+describe('test domains', () => {
+  test('main domain', () => {
+    const expected = {
+      'google.com': true,
+      'google.co.jp': true,
+      'example.com': false,
+      'example.net': false
+    }
 
-  for (const domain in expected) {
-    expect(googleSupportedDomains.test(domain)).toEqual(expected[domain])
-  }
-})
+    for (const domain in expected) {
+      expect(googleSupportedDomains.test(domain)).toEqual(expected[domain])
+    }
+  })
 
-test('test subdomains', () => {
-  const expected = {
-    'mail.google.com': true,
-    'drive.google.com': true,
-    'foo.example.com': false,
-    'bar.example.com': false
-  }
+  test('subdomains', () => {
+    const expected = {
+      'mail.google.com': true,
+      'drive.google.com': true,
+      'foo.example.com': false,
+      'bar.example.com': false
+    }
 
-  for (const domain in expected) {
-    expect(googleSupportedDomains.test(domain)).toEqual(expected[domain])
-  }
+    for (const domain in expected) {
+      expect(googleSupportedDomains.test(domain)).toEqual(expected[domain])
+    }
+  })
 })
